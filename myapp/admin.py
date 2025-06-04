3738520be54893f86e80202a7af7e9ff8a015222
JonathanUzielMedina22 <uziel.jonathan22@gmail.com>
2025-06-04 02:03:30 -0700

    reset

--- myapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Ganado_Vacuno
-
-class Ganado_Admin(admin.ModelAdmin):
-    readonly_fields = ("agregado", )
-
-# Register your models here.
-admin.site.register(Ganado_Vacuno, Ganado_Admin)
